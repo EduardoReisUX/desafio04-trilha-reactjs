8a60cffa69702b89451203c6b57c81ba804a64e8
EduardoReisUX <ducabruno09@hotmail.com>
2021-12-27 14:43:51 -0400

    add home page and its styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,44 +29,54 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <main className="container">
-      <div className="post">
-        <a href="#">
-          <h2>Como utilizar Hooks</h2>
-          <h4>Pensando em sincronização em vez de ciclos de vida.</h4>
-          <section className="info">
-            <time>15 Mar 2021</time>
-            <p>Joseph Oliveira</p>
-          </section>
-        </a>
+    <main className={styles.container}>
+      <div className={styles.post}>
+        <Link href="#">
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p className={styles.subtitle}>
+              Pensando em sincronização em vez de ciclos de vida.
+            </p>
+            <section className={styles.info}>
+              <FiCalendar /> <time>15 Mar 2021</time>
+              <FiUser /> <p>Joseph Oliveira</p>
+            </section>
+          </a>
+        </Link>
       </div>
-      <div className="post">
-        <a href="#">
-          <h2>Criando um app CRA do zero</h2>
-          <h4>
-            Tudo sobre como criar a sua primeira aplicação utilizando Create
-            React App
-          </h4>
-          <section className="info">
-            <time>19 Abr 2021</time>
-            <p>Danilo Vieira</p>
-          </section>
-        </a>
+      <div className={styles.post}>
+        <Link href="#">
+          <a>
+            <strong>Criando um app CRA do zero</strong>
+            <p className={styles.subtitle}>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <section className={styles.info}>
+              <FiCalendar /> <time>19 Abr 2021</time>
+              <FiUser /> <p>Danilo Vieira</p>
+            </section>
+          </a>
+        </Link>
       </div>
-      <div className="post">
-        <a href="#">
-          <h2>Como utilizar Hooks</h2>
-          <h4>Pensando em sincronização em vez de ciclos de vida.</h4>
-          <section className="info">
-            <time>15 Mar 2021</time>
-            <p>Joseph Oliveira</p>
-          </section>
-        </a>
+      <div className={styles.post}>
+        <Link href="#">
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p className={styles.subtitle}>
+              Pensando em sincronização em vez de ciclos de vida.
+            </p>
+            <section className={styles.info}>
+              <FiCalendar /> <time>15 Mar 2021</time>
+              <FiUser /> <p>Joseph Oliveira</p>
+            </section>
+          </a>
+        </Link>
       </div>
 
-      <a className="seeMore" href="#">
-        Carregar mais posts
-      </a>
+      <Link href="#">
+        <a className={styles.seeMore}>Carregar mais posts</a>
+      </Link>
     </main>
   );
 }
